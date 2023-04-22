632f599608a03a2c0b3049a7e883df2b78c74c43
agrshch <anatoly@grashchenko.ru>
2023-04-22 11:48:11 +0200

    refactored to a single shape with a single shader

--- gradient.frag
+++ gradient.frag
@@ -1,17 +1,65 @@
-precision mediump float;
+precision highp float;
 varying vec2 vTexCoord;
 
-float rand(vec2 co){
-    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+#define MAX 1000
+
+uniform vec3 u_pts [MAX];
+uniform int u_pts_n;
+uniform vec2 u_resolution;
+
+vec2 pointOnLineWithX(float x, vec2 a, vec2 b) {
+   float m = (b.y - a.y) / (b.x - a.x);
+   float y = m * (x - a.x) + a.y;
+   return vec2(x, y);
 }
 
 void main() {
-  vec2 coord = vTexCoord;
+  vec2 pix = vTexCoord;
+  pix.y = 1.0 - pix.y;
 
-  float r = rand(coord);
-  vec3 n = (vec3(r,r,r)*2.0 - vec3(r,r,r)) * 0.15;
-  float mask = coord.y;
-  vec3 gradient = mix(vec3(0.0), vec3(1.0), mask);
+  // 01. see if line segment shares x coord with pixel at some point
+  // 02. if yes — find the closest intersection
+  // 03. find is the interesection higher or lower
+  // 04. check the distance from pixel to intersection, save the record values/point
+  vec2  topClosest = vec2 (pix.x, 0.0), 
+        bottomClosest = vec2 (pix.x, 1.0);
+  float topRecord = pix.y, 
+        bottomRecord = 1.0 - pix.y;
+  for (int i = 1; i < MAX; i++){
+    if(i>=u_pts_n) break;
+    if (u_pts[i-1].z != u_pts[i].z) continue;
+    // define the line segment
+    vec2 a = u_pts[i-1].xy / u_resolution;
+    vec2 b = u_pts[i].xy / u_resolution;
+    // if no intersections - skip
+    if(!(a.x < pix.x && b.x > pix.x) && !(a.x > pix.x && b.x < pix.x)) continue; 
+    // find the intersection point
+    vec2 intersection = pointOnLineWithX(pix.x, a, b);
+    // is it higher or lower?
+    if (intersection.y <= pix.y) { // higher
+      //if it hits the record
+      if(pix.y - intersection.y < topRecord) {
+        topRecord = pix.y - intersection.y;
+        topClosest = intersection;
+      }
+    } else { // lower
+      //if it hits the record
+      if(intersection.y - pix.y < bottomRecord) {
+        bottomRecord = intersection.y - pix.y;
+        bottomClosest = intersection;
+      }
+    }
+  }
+  
+  // 06. save the distances in dist1 and dist2
+  float distTop = pix.y - topClosest.y;
+  float distBottom = bottomClosest.y - pix.y;
+  
+  // 07. find t = dist1 / (dist1+dist2)
+  float t = distTop / (distTop+distBottom);
 
-  gl_FragColor = vec4(gradient+n, 1.0 );
+  // 08. interpolate between two colors using t
+  vec3 gradient = mix(vec3(0.0), vec3(1.0), t);
+
+  gl_FragColor = vec4(gradient, 1.0 );
 }
--- sketch.js
+++ sketch.js
@@ -1,35 +1,22 @@
-p5.disableFriendlyErrors = true;
-
-let myShader;
-let COLS = 800;
-let breakPoints = [];
-let threshold = 0.5;
-let wasPreviouslyPressed = false;
-let clickIsBlocked = false;
-
-let customShape;
+let gradientShader,
+    CNV,
+    PTS = [],
+    segmentCounter = 0,
+    clickIsBlocked = false;
 
 function preload(){
-  myShader = loadShader('./basic.vert', './gradient.frag');
+  gradientShader = loadShader('./basic.vert', './gradient.frag');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  CNV = createCanvas(windowWidth, windowHeight, WEBGL);
+  CNV.parent(select('#cnv_div'));
   noLoop();
-  noStroke();
-  
-  
-  //resolution
-  // COLS = width;
-  let colSlider = createSlider(1,1000,width,1);
-  colSlider.position(20, 30);
-  colSlider.changed(changeCols);
-  colSlider.mouseOver(blockClicks);
-  colSlider.mouseOut(unblockClicks);
 
   let clearButton = createButton('clear');
-  clearButton.position(20, 70);
-  clearButton.mousePressed(wipe,breakPoints);
+  // clearButton.parent(select('#settings_div'))
+  clearButton.position(20, 20);
+  clearButton.mousePressed(()=> PTS=[]);
   clearButton.mouseOver(blockClicks);
   clearButton.mouseOut(unblockClicks);
 }
@@ -37,43 +24,16 @@ function setup() {
 function draw() {
   background(255);
   translate(-width/2, -height/2);
-
-  //draw rects
-  let w = width/COLS;
-  for (let i=0; i < COLS; i++){
-    //check if there are any breakpoints in the colomn. store findings into array
-    let currentBP = [];
-    for (let bp of breakPoints) if (bp.x > i*w && bp.x < (i+1)*w) currentBP.push(bp);
-    if(currentBP.length > 0) {
-      currentBP.sort( (a,b) => a.y - b.y );
-      let y = 0;
-      let prevPoint = new p5.Vector(0,-1000);
-      for (let bp of currentBP){
-        if (bp.y - prevPoint.y < w/2) continue;
-        let h = bp.y - y;
-        shader(myShader);
-        rect(i*w, y, w, h);
-        y = bp.y;
-        prevPoint = bp;
-      }
-      shader(myShader);
-      rect(i*w, y, w, height - y);
-    } else {
-      shader(myShader);
-      rect(i*w,0,w,height);
-    }
-  }
-
+  noStroke();
+  shader(gradientShader);
+  gradientShader.setUniform('u_pts', PTS);
+  gradientShader.setUniform('u_pts_n', floor(PTS.length/3));
+  gradientShader.setUniform('u_resolution', [width,height]);
+  rect(0,0,width,height);
 }
 
 function keyPressed() {
   if (key == ' ') save('Gradient_drawing.jpg');
-  if (key == '1') pixelDensity(1); redraw();
-  if (key == '2') pixelDensity(2); redraw();
-  if (key == '3') pixelDensity(3); redraw();
-  if (key == '4') pixelDensity(4); redraw();
-  if (key == '5') pixelDensity(5); redraw();
-  if (key == '6') pixelDensity(6); redraw();
 }
 
 function windowResized(){
@@ -81,47 +41,15 @@ function windowResized(){
   redraw();
 }
 
-function changeCols(){
-  COLS = floor(this.value());
-  threshold = width/COLS;
-  redraw();
-}
-
-//add breakPoints
+//add PTS
 function mousePressed(){
   if(clickIsBlocked)return;
-  breakPoints.push(new p5.Vector(mouseX, mouseY));
-  console.log('pressed');
-  redraw();
-  wasPreviouslyPressed = true;
+  segmentCounter++;
 }
 
 function mouseDragged(){
   if(clickIsBlocked)return;
-  //check if the button were pressed in previous frame. if not — add a breakpoint
-  if (!wasPreviouslyPressed) {
-    breakPoints.push(new p5.Vector(mouseX, mouseY));
-    wasPreviouslyPressed = true;
-    redraw();
-    return;
-  }
-
-  //if yes — check the dist to previous breakpoint;
-  let prev = breakPoints.at(-1);
-  let curr = new p5.Vector(mouseX, mouseY);
-  let d = distSquared(prev, curr);
-
-  //if it's less than threshold — do nothing.
-  if (dist < threshold*threshold) return;
-
-  //если больше — смотрим на округленное значение расстояние/порог и рисуем столько точек, сколько наокругляли
-  let number = Math.round(sqrt(d)/threshold);
-  let step = 1/number;
-  for (let i = step; i <= 1; i+=step){
-    i = constrain(i,0,1);
-    breakPoints.push(p5.Vector.lerp(prev, curr, i));
-  }
-
+  PTS.push(mouseX, mouseY,segmentCounter);
   redraw();
 }
 
@@ -136,9 +64,6 @@ function distSquared(a,b){
   return dx*dx + dy*dy;
 }
 
-function wipe(values){
-  breakPoints = [];
-}
 
 function blockClicks(){
   clickIsBlocked = true;
